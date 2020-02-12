cad0bb4b216ec12ed0566429ad6aedbf3c80afe4
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-02-11 19:57:33 -0500

    creating view for new meeting

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,16 +1,41 @@
 class MeetingsController < ApplicationController
   def new
+    @meeting = Meeting.new
   end
 
   def create
+    @meeting = Meeting.new(meeting_params)
+    @user = current_user
+    if @meeting.save
+      redirect_to meeting_path(@meeting)
+    else
+      render :new
+    end
   end
 
   def show
-  end
-
-  def update
+    @meeting = Meeting.find(params[:id])
   end
 
   def destroy
+    @meeting = Meeting.find(params[:id])
+    @meeting.destroy
+    redirect_to meetings_path
+  end
+
+  def edit
+    @meeting = Meeting.find(params[:id])
+    @meeting.save
+  end
+
+  def update
+    @meeting = Meeting.find(params[:id])
+    @meeting.update(meeting_params)
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:start_time, :end_time, :location, :accepted, :mentor, :mentee)
   end
 end
