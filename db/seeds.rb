f378db6d0fe4ae9c029a8b0e9cdc1f50f2adc3ed
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-02-13 20:52:09 -0500

    preparing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+require "open-uri"
+
+skills = Skill.create([{ name: 'Ruby' }, { name: 'JavaScript' }, { name: 'HTML & CSS' }, { name: 'UI/UX' }, { name: 'Product Management'}, { name: 'PHP'}, { name: 'Angular'}])
+file = URI.open('https://static-showpoblog.executiveponies.com/uploads/sites/1/2018/03/the-office-revival-nbc-steve-carell-replaced.png')
+
+
+20.times do
+  user = User.create({
+    email: Faker::Internet.email,
+    password: 'guest1',
+    name: Faker::Name.name,
+    description: Faker::TvShows::MichaelScott.quote,
+    photo:
+  })
+
+  user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+  user.experiences.create({
+      skill: skills.sample,
+      years_of_experience: [1..10].sample
+    })
+end
+
+
