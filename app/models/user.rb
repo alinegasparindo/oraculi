2b15a6d1f61ef2f081ed3119332909fb9bf7be35
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-02-13 21:39:48 -0500

    seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :experiences, dependent: :destroy
-  has_many :availabilities, dependent: :destroy
+  # has_many :availabilities, dependent: :destroy
   has_many :skills, through: :experiences
   has_one_attached :photo
   has_many :mentor_meetings, foreign_key: 'mentor_id', class_name: 'Meeting', dependent: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Skill.destroy_all
+User.destroy_all
+
 
 require 'faker'
 require "open-uri"
 
 skills = Skill.create([{ name: 'Ruby' }, { name: 'JavaScript' }, { name: 'HTML & CSS' }, { name: 'UI/UX' }, { name: 'Product Management'}, { name: 'PHP'}, { name: 'Angular'}])
-file = URI.open('https://static-showpoblog.executiveponies.com/uploads/sites/1/2018/03/the-office-revival-nbc-steve-carell-replaced.png')
-
 
 20.times do
-  user = User.create({
+  file = URI.open('https://static-showpoblog.executiveponies.com/uploads/sites/1/2018/03/the-office-revival-nbc-steve-carell-replaced.png')
+
+  name = Faker::Name.unique.name
+
+  user = User.new({
     email: Faker::Internet.email,
     password: 'guest1',
-    name: Faker::Name.name,
-    description: Faker::TvShows::MichaelScott.quote,
-    photo:
+    name: name,
+    description: Faker::TvShows::MichaelScott.quote
   })
 
-  user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-
-  user.experiences.create({
-      skill: skills.sample,
-      years_of_experience: [1..10].sample
-    })
+  user.photo.attach(io: file, filename: "#{name}.png", content_type: 'image/png')
+  user.save!
 end
 
-
+  # user.experiences.create({
+  #     skill: skills.sample,
+  #     years_of_experience: [1..10].sample
+  #   })
