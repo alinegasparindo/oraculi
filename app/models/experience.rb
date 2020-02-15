c7fe0989df046be51c7350ac0d900e1c160f3c34
Jeff Trempe <trempe.jeff@gmail.com>
2020-02-15 15:16:23 -0500

    Added validation to the experiences/new

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,7 @@
 class Experience < ApplicationRecord
+  validates :skill, :years_of_experience, presence: true
+  validates_uniqueness_of :skill, scope: :user
+
   belongs_to :user
   belongs_to :skill
 end
