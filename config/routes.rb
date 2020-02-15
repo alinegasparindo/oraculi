942da3598efe2a9006bebc676be27239010884c4
Jeff Trempe <trempe.jeff@gmail.com>
2020-02-15 11:20:41 -0500

    Added the .env

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: :show do
-<<<<<<< HEAD
-    resources :meetings, only: %i(new create)
-    resources :experiences
-=======
     resources :meetings, only: %i(new create show index)
->>>>>>> c4dcbd0ff52c795d1ff16054c853c4f813287935
+    resources :experiences
   end
 
   # resources :meetings, only: %i(update destroy)
