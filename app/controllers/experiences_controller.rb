7a48e0b05aa8a98797d8c90be24f89782fe02178
Lotfi Kaced <lotfikaced@gmail.com>
2020-02-15 13:08:55 -0500

    Css on home page and header, plus search form

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,16 +1,38 @@
 class ExperiencesController < ApplicationController
+  before_action :me
+
   def new
+    @experience = Experience.new
   end
 
   def create
+    @experience = Experience.new(experience_params)
+    if @experience.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+    @experience = Experience.find(params[:id])
+    @experience.destroy
+    redirect_to user_path(@user)
   end
 
   def index
+  end
+
+  private
+
+  def me
+    @user = current_user
+  end
+
+  def experience_params
+    params.require(:experience).permit(%i[user skill years_of_experience])
   end
 end
