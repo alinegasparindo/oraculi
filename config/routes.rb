97567199c3c0a14042bf90a98917703a2ab97506
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-02-22 11:35:04 -0500

    Revert "Merge pull request #21 from alinegasparindo/reviews"
    This reverts commit 85ee2eb0c9ca0f3af8050d07741ecf39fb42a0b8, reversing
    changes made to bcacc75f47a0aa3ed977110cd79f082f0f486d8c.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
 
   get 'user', to: 'pages#profile'
-#21 from alinegasparindo/reviews
+
   get 'meeting', to: 'meeting#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
